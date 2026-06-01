bd0a330b9f7ef79ef379c6a53b5b7a6b1d0a383e
asxiuixu <licheng1996121@gmail.com>
2026-06-01 17:51:54 +0800

    build(deps): downgrade mimalloc 2.2.4 → 2.1.7

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class EntelechyConan(ConanFile):
         self.requires("glfw/3.4")
         self.requires("glad/0.1.36")
         self.requires("imgui/1.91.0-docking")
-        self.requires("mimalloc/2.2.4")
+        self.requires("mimalloc/2.1.7")
         # Future dependencies:
         # self.requires("spdlog/1.14.1")
         # self.requires("glm/1.0.1")
